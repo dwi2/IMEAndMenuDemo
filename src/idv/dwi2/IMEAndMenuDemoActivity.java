aae4e16e49fc89dd052313eb03b4a5737f97da04
dwi2 <dwi019@gmail.com>
2011-08-10 16:17:02 +0800

    add context menu

--- src/idv/dwi2/IMEAndMenuDemoActivity.java
+++ src/idv/dwi2/IMEAndMenuDemoActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
@@ -20,37 +23,39 @@ public class IMEAndMenuDemoActivity extends Activity {
 
 	Menu theMenu = null;
 	private boolean isPasswordVisible;
-	public static String TAG="IMEAndMenuDemoActivity";
-	
-	public void makeToast(String msg)
-	{
-		Log.i(TAG, "makeToast "+msg);
+	public static String TAG = "IMEAndMenuDemoActivity";
+
+	public void makeToast(String msg) {
+		Log.i(TAG, "makeToast " + msg);
 		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		editText2 = (EditText) findViewById(R.id.editText2);
-		editText2.setOnEditorActionListener(
-				new BasicEditorActionListener("送!!"));
+		editText2
+				.setOnEditorActionListener(new BasicEditorActionListener("送!!"));
 		editText3 = (EditText) findViewById(R.id.editText3);
-		editText3.setOnEditorActionListener(
-				new BasicEditorActionListener());
+		editText3.setOnEditorActionListener(new BasicEditorActionListener());
 		editText6 = (EditText) findViewById(R.id.editText6);
-		editText6.setOnEditorActionListener(new BasicEditorActionListener("去!!"));
+		editText6
+				.setOnEditorActionListener(new BasicEditorActionListener("去!!"));
 		isPasswordVisible = false;
-	}
-	
-//	@Override
-//	public void onCreateContextMenu(ContextMenu menu, View v,
-//			ContextMenuInfo menuInfo) {
-//		// TODO Auto-generated method stub
-//		super.onCreateContextMenu(menu, v, menuInfo);
-//	}
 
-	
+		registerForContextMenu(findViewById(R.id.scrollView1));
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		Log.i(TAG, "onCreateContextMenu()");
+		theMenu = menu;
+		new MenuInflater(getApplication()).inflate(R.menu.option_menu, menu);
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+
 	// option menu
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,51 +66,64 @@ public class IMEAndMenuDemoActivity extends Activity {
 	}
 
 	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		Log.i(TAG, "onContextItemSelected()");
+		handleMenuSelection(item);
+		return super.onContextItemSelected(item);
+	}
+
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Log.i(TAG, "onOptionsItemSelected()");
-		Log.i(TAG, "item.getItemId()="+item.getItemId());
-		
-		switch (item.getItemId())
-		{
-			case R.id.visible_password:
-				isPasswordVisible = true;
-				setPasswordVisibility();
-				break;
-			case R.id.invisible_password:
-				isPasswordVisible = false;
-				setPasswordVisibility();
-				break;
-			case R.id.submenu:
-				if (theMenu != null){
-					MenuItem checkedItem;
-					if (isPasswordVisible) {
-						Log.i(TAG, "isPasswordVisible="+isPasswordVisible+" set R.id.visible_password as checked");
-						checkedItem = (MenuItem)theMenu.findItem(R.id.visible_password);
-					}
-					else {
-						Log.i(TAG, "isPasswordVisible="+isPasswordVisible+" set R.id.invisible_password as checked");
-						checkedItem = (MenuItem)theMenu.findItem(R.id.invisible_password);
-					}
-					checkedItem.setChecked(true);
-				}
-				break;
-			case R.id.close:
-				break;
-		}
+		Log.i(TAG, "item.getItemId()=" + item.getItemId());
+		handleMenuSelection(item);
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void setPasswordVisibility()
-	{
-		Log.i(TAG, "setPasswordVisibility()");
-		Log.i(TAG, "set password visibility to "+isPasswordVisible);
+	private void handleMenuSelection(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.visible_password:
+			isPasswordVisible = true;
+			setupPasswordVisibility();
+			break;
+		case R.id.invisible_password:
+			isPasswordVisible = false;
+			setupPasswordVisibility();
+			break;
+		case R.id.submenu:
+			if (theMenu != null) {
+				MenuItem checkedItem;
+				if (isPasswordVisible) {
+					Log.i(TAG, "isPasswordVisible=" + isPasswordVisible
+							+ " set R.id.visible_password as checked");
+					checkedItem = (MenuItem) theMenu
+							.findItem(R.id.visible_password);
+				} else {
+					Log.i(TAG, "isPasswordVisible=" + isPasswordVisible
+							+ " set R.id.invisible_password as checked");
+					checkedItem = (MenuItem) theMenu
+							.findItem(R.id.invisible_password);
+				}
+				checkedItem.setChecked(true);
+			}
+			break;
+		case R.id.exit_app:
+			finish();
+			break;
+		case R.id.close:
+			break;			
+		}
+	}
+
+	private void setupPasswordVisibility() {
+		Log.i(TAG, "setupPasswordVisibility()");
+		Log.i(TAG, "set password visibility to " + isPasswordVisible);
 		if (editText6 == null)
 			editText6 = (EditText) findViewById(R.id.editText6);
 		int inputType = InputType.TYPE_CLASS_TEXT;
 		if (isPasswordVisible) {
 			inputType |= InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD;
-		}
-		else {
+		} else {
 			inputType |= InputType.TYPE_TEXT_VARIATION_PASSWORD;
 		}
 		editText6.setInputType(inputType);
@@ -113,21 +131,21 @@ public class IMEAndMenuDemoActivity extends Activity {
 
 	class BasicEditorActionListener implements OnEditorActionListener {
 		String toastMessage;
-		
+
 		BasicEditorActionListener() {
 			toastMessage = "好!!";
 		};
-		
+
 		BasicEditorActionListener(String msg) {
 			toastMessage = msg;
 		};
-		
+
 		@Override
 		public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-			Log.i(TAG, "actionId="+actionId);
+			Log.i(TAG, "actionId=" + actionId);
 			makeToast(toastMessage);
 			return false;
 		}
-		
+
 	}
 }
